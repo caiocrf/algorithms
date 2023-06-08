f01c83ad1eaf8cfc99fd88144dc04d646b998b13
caio pinho <icandoit2026@gmail.com>
2023-06-08 14:42:00 -0300

    req 1 and 2 finished

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui!"""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    counter = 0
+
+    if not all(isinstance(s, int) and isinstance(e, int) for s, e in permanence_period):
+        return None
+
+    for entry_time, exit_time in permanence_period:
+        if entry_time <= target_time <= exit_time:
+            counter += 1
+
+    return counter
+
