fe869ad0c73e7961dd7668ebeeb164c0d49e5371
caio pinho <icandoit2026@gmail.com>
2023-06-09 11:54:09 -0300

    .

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,33 +1,24 @@
-  
-def division(array, low, high):
-        pivot_index = low
-        pivot_value = array[high]
+def quicksort(array):
+    if len(array) <= 1:
+        return array
 
-        for j in range(low, high):
-          if array[j] <= pivot_value:
-            array[pivot_index], array[j] = array[j], array[pivot_index]
-            pivot_index += 1
+    pivot = array[len(array) // 2]
+    left = [x for x in array if x < pivot]
+    middle = [x for x in array if x == pivot]
+    right = [x for x in array if x > pivot]
 
-        array[pivot_index], array[high] = array[high], array[pivot_index]
-
-        return pivot_index
-
-def sort(array, low, high):
-    if low < high:
-     index = division(array, low, high)
-     sort(array, low, index - 1)
-     sort(array, index + 1, high)
+    return quicksort(left) + middle + quicksort(right)
 
 
 def is_anagram(first_string, second_string):
     first_str = list(first_string.lower())
     second_str = list(second_string.lower())
 
-    sort(first_str, 0, len(first_str) - 1)
-    sort(second_str, 0, len(second_str) - 1)
+    sorted_first = quicksort(first_str)
+    sorted_second = quicksort(second_str)
 
-    first = "".join(first_str)
-    second = "".join(second_str)
+    first = "".join(sorted_first)
+    second = "".join(sorted_second)
 
     if not first_string or not second_string:
         return (first, second, False)
